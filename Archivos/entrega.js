bf73076a17676eda328c91cddfb73687744f441d
Cristian <crisgianoboli@gmail.com>
2022-06-30 19:05:57 -0300

    trabajo con entregable de archivos

--- Archivos/entrega.js
+++ Archivos/entrega.js
@@ -1,28 +1,49 @@
+const fs = require("fs");
+
 class Contenedor {
-  constructor(name, item) {
-    this.name = name;
-    this.item = [item];
+  constructor(file) {
+    this.file = file;
   }
 
-  saveData(description, price) {
-    this.item.push({
-      description: description,
-      price: price,
-    });
+  async saveData(item) {
+    const products = await this.getAllObject();
+    let newId;
+    if (products.length == 0) {
+      newId = 1;
+    } else {
+      newId = products[products.length - 1].id + 1;
+    }
+    const newProduct = { ...item, id: newId };
+    products.push(newProduct);
+    try {
+      await fs.promises.writeFile(this.file, JSON.stringify(products, null, 2));
+      return newId;
+    } catch (error) {
+      return console.log(error);
+    }
   }
 
   getObjetctById() {}
 
-  getAllObject() {}
+  async getAllObject() {
+    try {
+      const items = await fs.promises.readFile(this.file, "utf-8");
+      return JSON.parse(items);
+    } catch (error) {
+      return [];
+    }
+  }
 
   deleteObjectById() {}
 
   deleteAllObject() {}
 }
 
-const item = new Contenedor("Phone", {
-  description: "Iphone 8 plus",
-  price: 750,
-});
+const product = new Contenedor("product.txt");
 
-console.log(item);
+product.saveData({
+  title: "Escuadra",
+  price: 123.45,
+  thumbnail:
+    "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+});
